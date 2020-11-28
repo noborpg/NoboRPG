90ba78971a53250653e6b6c6aa6f4d894314a779
Lognes <lognes.kek@gmail.com>
2020-11-28 16:25:30 +0000

    Setting up the combat system

---
+++ project/js/plugins/Draw Battle Commands Horizontally.js
@@ -0,0 +1,11 @@
+Window_ActorCommand.prototype.numVisibleRows = function() {
+    return 1;
+};
+
+Window_ActorCommand.prototype.maxCols = function() {
+    return 3;
+};
+
+Window_ActorCommand.prototype.windowWidth = function() {
+    return 1280;
+};
